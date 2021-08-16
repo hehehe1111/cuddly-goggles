61086b291f4e220bec2c7f57bb2cf93e35a39102
hemantafpv <87780753+hemantafpv@users.noreply.github.com>
2021-08-16 00:05:32 -0400

    Create random.js

---
+++ random.js
@@ -0,0 +1,12 @@
+function housekeeper (name, age, hasworkpermit, languages){
+    this.name = name;
+    this.age = age;
+    this.hasworkpermit = hasworkpermit;
+    this.languages = languages;
+}
+var housekeeper1 = {
+    yearsOfExperience: 12,
+    name: "Jane",
+    cleaningRep: ["bathroom","lobby","bedroom"]
+}
+console.log(housekeeper1.name)
